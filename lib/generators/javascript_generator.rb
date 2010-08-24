86a2a0903f5171c43df41572408fa124c9cefc08
pch <piotr@chmolowski.pl>
2010-08-24 14:22:23 +0200

    test

--- lib/generators/javascript_generator.rb
+++ lib/generators/javascript_generator.rb
@@ -1,10 +1,12 @@
 module ValidatedFields
   class JavascriptGenerator < Rails::Generators::Base
-    source_root File.expand_path("../javascripts", __FILE__)
+    desc "This generator copies the validated-fields.js file to the public/javascripts directory in your application."
+    
+    #source_root File.expand_path("../javascripts", __FILE__)
 
     # all public methods in here will be run in order
     def copy_javascript_file
-      copy_file('validated-fields.js', 'public/javascripts/validated-fields.js')
+      #copy_file('validated-fields.js', 'public/javascripts/validated-fields.js')
     end
   end
 end
