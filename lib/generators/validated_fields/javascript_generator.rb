ccecf9b117c18803e96c1cfdb661b5d5f5efb4d0
pch <piotr@chmolowski.pl>
2010-08-24 14:43:11 +0200

    Trying different namespace

--- lib/generators/validated_fields/javascript_generator.rb
+++ lib/generators/validated_fields/javascript_generator.rb
@@ -2,7 +2,7 @@ module ValidatedFields
   class JavascriptGenerator < Rails::Generators::Base
     desc "This generator copies the validated-fields.js file to the public/javascripts directory in your application."
     
-    source_root File.expand_path("../javascripts", __FILE__)
+    source_root File.expand_path("../../javascripts", __FILE__)
 
     # all public methods in here will be run in order
     def copy_javascript_file
