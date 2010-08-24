e23f4e5706ca76afe853e8d7d2119d2527edb3aa
pch <piotr@chmolowski.pl>
2010-08-24 09:05:54 +0200

    Add greater_than_or_equal_to and greater_than_or_equal_to support to NumericalityValidator

--- lib/validated_fields/validators/numericality_validator.rb
+++ lib/validated_fields/validators/numericality_validator.rb
@@ -12,6 +12,9 @@ module ValidatedFields
         options[:min] = voptions[:greater_than].to_i + 1 if voptions[:greater_than].present?
         options[:max] = voptions[:less_than].to_i    - 1 if voptions[:less_than].present?
         
+        options[:min] = voptions[:greater_than_or_equal_to].to_i + 1 if voptions[:greater_than_or_equal_to].present?
+        options[:max] = voptions[:less_than_or_equal_to].to_i    - 1 if voptions[:less_than_or_equal_to].present?
+        
         options['data-numericality-error-msg'] = voptions[:message] if voptions[:message].present?
         options
       end
