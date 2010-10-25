6629fdac28a0580975faa7d1e5871db6e3fac934
Roman Shterenzon <roman.shterenzon@gmail.com>
2010-10-26 01:03:51 +0200

    Add support for format checking

--- lib/validated_fields/validators/format_validator.rb
+++ lib/validated_fields/validators/format_validator.rb
@@ -6,16 +6,17 @@ module ValidatedFields
     module FormatValidator
 
       def self.prepare_options(validator, options)
+        voptions = validator.options
         # TODO: tidy up regexes for javascript compatibility
-        options[:pattern] = validator.options[:with].inspect
+        options[:pattern] = voptions[:with].inspect
 
-        options["data-allow-blank"]  = true if validator.options[:allow_nil].present?   && validator.options[:allow_nil]   == true
-        options["data-allow-blank"]  = true if validator.options[:allow_blank].present? && validator.options[:allow_blank] == true
+        options["data-allow-blank"]  = true if voptions[:allow_nil].present?   && voptions[:allow_nil]   == true
+        options["data-allow-blank"]  = true if voptions[:allow_blank].present? && voptions[:allow_blank] == true
 
-        options["data-error-format"] = validator.options[:message] if validator.options[:message].present?
+        options["data-error-format"] = validator.options[:message] || 'Invalid format'
         options
       end
     end
-    
+
   end
 end
